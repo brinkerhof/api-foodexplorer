2122eec51b466dbbc81a447e91c459c4c1d01bc8
brinkerhof <murilocabral8@gmail.com>
2022-11-08 20:15:18 -0300

    User route fully added

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,5 @@
 import knex from "../database//knex/index.js";
+import AppError from "../utils/AppError.js";
 
 export default class UsersController {
   async index(req, res) {
@@ -6,8 +7,57 @@ export default class UsersController {
 
     return res.json(users);
   }
-  async show(req, res) {}
-  async create(req, res) {}
-  async update(req, res) {}
-  async delete(req, res) {}
+  async show(req, res) {
+    const user_id = req.params.id;
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    return res.json(user);
+  }
+  async create(req, res) {
+    const { name, email, password, isAdmin } = req.body;
+
+    const emailVerifyIfExists = await knex("users").where({ email }).first();
+
+    if (!emailVerifyIfExists) {
+      throw new AppError("User already exists", 404);
+    }
+
+    await knex("users").insert({ name, email, password, isAdmin });
+
+    return res.json("User successfully created");
+  }
+  async update(req, res) {
+    const { name, email, password, isAdmin } = req.body;
+    const user_id = req.params.id;
+
+    const emailVerifyIfExists = await knex("users").where({ email }).first();
+
+    if (!emailVerifyIfExists) {
+      throw new AppError("User already exists", 404);
+    }
+
+    await knex("users").update({ name, email, password, isAdmin });
+
+    const userUpdated = await knex("users").where({ id: user_id });
+
+    return res.json(userUpdated);
+  }
+  async delete(req, res) {
+    const user_id = req.params.id;
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    await knex("users").where({ id: user_id }).del();
+
+    return res.json("Success delete");
+  }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersController = new UsersController();
 const usersRoutes = Router();
 
 usersRoutes.get("/", usersController.index);
+usersRoutes.get("/:id", usersController.show);
 
 export default usersRoutes;
