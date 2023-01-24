d72981a1fbe68f0558831ee9f93ddefb55026483
brinkerhof <murilocabral8@gmail.com>
2023-01-24 12:44:13 -0300

    Done

--- src/database/knex/migrations/20221108143523_createIngredients.js
+++ src/database/knex/migrations/20221108143523_createIngredients.js
@@ -9,7 +9,6 @@ export function up(knex) {
         )
       );
     table.string("name").notNullable();
-    table.string("image").notNullable();
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
