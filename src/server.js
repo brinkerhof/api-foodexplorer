5fa4b1b2d18d9065c225af341e219ac46321b63b
brinkerhof <murilocabral8@gmail.com>
2023-04-25 14:42:06 -0300

    cors up

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import { json } from "express";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
